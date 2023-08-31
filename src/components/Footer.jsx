649973750e15cc1b49c817db194c13342f0b91ef
swainrocky5 <45192363+swainrocky5@users.noreply.github.com>
2023-08-31 21:56:01 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>PixelVibe</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/samarveera" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/samarjitswain" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import vg from "../assets/2.Webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>PixelVibe</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+      <div className="home2">
+        <img src={vg} alt="graphics" />
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            "At PixelVibe, we specialize in crafting digital experiences that
+            resonate. As a forward-thinking tech company, we fuse design and
+            innovation to bring vibrancy to the digital realm. Our adept team
+            harnesses pixels and code to create immersive websites, intuitive
+            apps, and captivating visual content. With an unwavering commitment
+            to quality, we transform ideas into pixel-perfect realities. From
+            user-centered designs to cutting-edge solutions, we infuse each
+            project with creativity and expertise. At PixelVibe, we don't just
+            create products; we craft experiences that evoke emotions and leave
+            lasting impressions. Join us in shaping the digital landscape with
+            innovation, aesthetics, and functionality."
+          </p>
+        </div>
+      </div>
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div style={{ animationDelay: "0.3s" }}>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div style={{ animationDelay: "0.5s" }}>
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div style={{ animationDelay: "0.7s" }}>
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+            <div style={{ animationDelay: "1s" }}>
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
